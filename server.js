d3672a14f409ff5889c922ca30945528448042df
Samantha <samantha@Samanthas-MBP.lan>
2024-05-13 13:05:59 -0700

    install Google Oauth and Passport

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 require("dotenv").config();
 require('./config/db.connection.js')
-
-const { PORT } = process.env;
-
-const express = require("express");
+require('./config/database');
+require('./config/passport');
 
 const app = express();
+const { PORT } = process.env;
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 
-
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.get("/", (req, res) => {
     res.send("hello world");
 });
