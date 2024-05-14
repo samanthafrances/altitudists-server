90e500284db8bb0c126af057b1c96f7aa23f987b
Samantha <samantha@Samanthas-MBP.lan>
2024-05-13 21:21:38 -0700

    update passport

--- models/user.js
+++ models/user.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
- const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
+const User = require('../models/user');
 
- const userSchema = new Schema({
-   name: String
- }, {
-   timestamps: true
- });
+const userSchema = new Schema({
+  name: String,
+  googleId: {
+    type: String,
+    required: true
+  },
+  email: String,
+  avatar: String
+}, {
+  timestamps: true
+});
+
+ 
+
 
  module.exports = mongoose.model('User', userSchema);
