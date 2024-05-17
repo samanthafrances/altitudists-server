c3ad1a19d8c04d3bc540d2651cae520510a1011f
Samantha <samantha@Samanthas-MBP.lan>
2024-05-17 00:01:19 -0700

    connect frontend and backend socket

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ const io = require('socket.io') (server, {
 
 io.on("connection", (socket) => {
   console.log("connected to socket.io");
+
+  socket.on( "setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
 });
 
 
