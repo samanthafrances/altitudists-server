7e1de74f4139ac26e84c881a7483e1190132cab9
Samantha <samantha@Samanthas-MBP.lan>
2024-05-16 21:00:45 -0700

    update server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 
+const userProfileRouter = require("./routes/UserProfile.js");
+const AuthRouter = require("./routes/AuthRouter");
+const destinationsRouter = require("./routes/destinations.js");
+const chatRouter = require("./routes/chat.js");
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -24,6 +30,18 @@ app.use(passport.session());
 app.get("/", (req, res) => {
     res.send("hello world");
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+app.use("/auth", AuthRouter);
+app.use("/destinations", destinationsRouter);
+app.use("/chat", chatRouter);
+app.use("/userProfile", userProfileRouter);
+
 
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
