b38a4af9d5ba7d7700abd5e74925e99b7039e8ab
Samantha <samantha@Samanthas-MBP.lan>
2024-05-16 20:44:08 -0700

    configure User Profile model and route

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 module.exports = {
-    Ride: require("./ride"),
     User: require("./user"),
-    Ticket: require("./ticket"),
-    Schedule: require("./schedule"),
+    Destinations: require("./destination"),
+    Chat: require("./chat"),
   };
   
