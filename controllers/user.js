ce88f1998fa40a2affa28302d018025526d2805f
Samantha <samantha@Samanthas-MBP.lan>
2024-05-19 15:03:03 -0700

    update user controller registration and authentication

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,63 @@
 const express = require("express");
 const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
 
 
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
 
-module.exports = {
-   
-  };
-  
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Submit Information");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found");
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      photo: user.photo,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Submission");
+  }
+});
+
+module.exports = { allUsers, registerUser, authUser };
