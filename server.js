5078ac73f7fc6544a111cf3787c004bce607db42
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 12:53:39 -0700

    update server.js

--- server.js
+++ server.js
@@ -15,14 +15,6 @@ const session = require('express-session');
 const passport = require('passport');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const http = require('http');
-const server = http.createServer(app);
-const io = require('socket.io')(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin:"http://localhost:4000",
-  },
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -61,23 +53,50 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
+const PORT = process.env.PORT
 
-const PORT = process.env.PORT 
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`)
+);
+
+const io = require('socket.io')(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:4000",
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("connected to socket.io");
-
-  socket.on( "setup", (userData) => {
+  socket.on("setup", (userData) => {
     socket.join(userData._id);
-    console.log(userData._id);
     socket.emit("connected");
   });
 
   socket.on('join chat', (room) => {
     socket.join(room);
     console.log("User Joined Room: " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 
 });
-});
-
-server.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
