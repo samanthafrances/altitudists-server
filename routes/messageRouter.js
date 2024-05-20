3c2c1f9d7b8fc642e2430d55ddce676538e1a3dd
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 07:10:14 -0700

    update chat controller access chat functionality and message routes

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const {
+    allMessages,
+    sendMessage,
+  } = require("../controllers/message");
 const router = express.Router();
+
+router.route("/:chatId").get(protect, allMessages);
+router.route("/").post(protect, sendMessage);
 
 module.exports = router;
