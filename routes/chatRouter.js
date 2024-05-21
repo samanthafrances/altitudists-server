082c34abe5fc5a37dea285b9ee48de78d65614bc
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 20:24:55 -0700

    update chat routes callback functions

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -11,11 +11,23 @@ const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/").post(protect, accessChat);
-router.route("/").get(protect, fetchChats);
-router.route("/group").post(protect, createGroupChat);
-router.route("/rename").put(protect, renameGroup);
-router.route("/groupremove").put(protect, removeFromGroup);
-router.route("/groupadd").put(protect, addToGroup);
+router.post("/", protect, (req, res) => {
+  accessChat(req, res);
+});
+router.get("/", protect, (req, res) => {
+  fetchChats(req, res);
+});
+router.post("/group", protect, (req, res) => {
+  createGroupChat(req, res);
+});
+router.put("/rename", protect, (req, res) => {
+  renameGroup(req, res);
+});
+router.put("/groupremove", protect, (req, res) => {
+  removeFromGroup(req, res);
+});
+router.put("/groupadd", protect, (req, res) => {
+  addToGroup(req, res);
+});
 
 module.exports = router;
