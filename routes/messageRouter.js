aa57cf6bf982eed3d56cf740adfce4c8e1375280
Samantha <samantha@Samanthas-MBP.lan>
2024-05-20 20:31:30 -0700

    update message routes

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const {
-    allMessages,
-    sendMessage,
-  } = require("../controllers/message");
+const { allMessages, sendMessage } = require("../controllers/message");
+const { protect } = require("../middleware/auth");
+
 const router = express.Router();
 
-router.route("/:chatId").get(protect, allMessages);
-router.route("/").post(protect, sendMessage);
+router.get("/:chatId", protect, (req, res) => {
+  allMessages(req, res);
+});
+
+router.post("/", protect, (req, res) => {
+  sendMessage(req, res);
+});
 
 module.exports = router;
