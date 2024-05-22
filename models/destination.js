c0cb38075dbebe4fe689e300d4abca3e7804f5e1
Samantha <samantha@Samanthas-MBP.lan>
2024-05-22 16:56:50 -0700

    update destination model

---
+++ models/destination.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DestinationSchema = new Schema({
+  
+
+});
+
+module.exports = mongoose.model("Destination", DestinationSchema)
