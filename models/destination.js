09eb32478b34569608eb7eedd51cf4a4ddb47008
Samantha <samantha@Samanthas-MBP.lan>
2024-05-23 17:52:51 -0700

    debug auth

--- models/destination.js
+++ models/destination.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const DestinationSchema = new Schema({
-  
+    name: String,
+    address: String,
+    lat: Number,
+    long: Number,
+    description: String,
+    image: String,
+    season: String, 
+    website: String,
 });
 
 module.exports = mongoose.model("Destination", DestinationSchema)
--- services/Auth.js
+++ services/Auth.js
@@ -1,4 +1,4 @@
-import Client from "./api";
+import { Client } from './api';
 
 export const RegisterUser = async (data) => {
     try {
