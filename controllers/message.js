d2caec4f509f7437aa7c41b5585da5df70465d66
Samantha <samantha@Samanthas-MBP.lan>
2024-05-24 11:42:56 -0700

    remove chat and message corrupt files

--- controllers/message.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Message = require("../models/message");
-const Chat = require("../models/chat");
-const User = require("../models/user");
-
-module.exports = { 
-    
- };
