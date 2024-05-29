39bd340f5762c3c38f62dcb5420b08b316b78af3
Samantha <samantha@Samanthas-MBP.lan>
2024-05-29 08:29:35 -0700

    updated destination route

--- routes/destinationsRouter.js
+++ routes/destinationsRouter.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Destination = require('../models/destination');
 
-router.get('/', (req, res) => {
-  res.send('Hello, World!');
+router.post('/', async (req, res) => {
+  try {
+    const { name, address, website } = req.body;
+    const destination = new Destination({ name, address, website });
+    await destination.save();
+    res.status(201).send(`Destination created successfully!`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const destinations = await Destination.find().exec();
+    res.json(destinations);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
